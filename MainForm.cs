e8e15e0dcdf37162d9485bc3c01d9809bed261d1
Samuel Holland <samuel@sholland.org>
2015-12-22 17:24:32 -0600

    Initial solution created by Visual Studio

---
+++ MainForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _7DaysBalanceTool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
